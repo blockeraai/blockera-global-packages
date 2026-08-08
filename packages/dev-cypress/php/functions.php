c6ee409b44c3102410138dfc73977c3f0e16a09d
rezaelahidev <rezaelahi.dev@gmail.com>
2026-08-08 23:56:43 +0330

    build: add composer manifests for dev-cypress and dev-tools

---
+++ packages/dev-cypress/php/functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Blockera Dev Cypress Helper Functions
+ *
+ * This file provides auxiliary PHP functions used for Cypress-based end-to-end testing
+ * within the Blockera development environment.
+ *
+ * @package Blockera\DevCypress
+ */
---
+++ packages/dev-tools/php/functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Blockera Dev Tools Helper Functions
+ *
+ * This file provides auxiliary PHP functions used for development tools
+ * within the Blockera development environment.
+ *
+ * @package Blockera\DevTools
+ */
