fce6c3bdc8a222d761e3216506c02ee1b6ae5d8d
Ali Aghdam <betteraliaghdam@gmail.com>
2026-08-13 01:46:52 +0330

    test(utils): cover Site Editor history patch

---
+++ packages/utils/js/site-editor/test/history-patch.spec.js
@@ -0,0 +1,82 @@
+/**
+ * @jest-environment jsdom
+ */
+
+/**
+ * External dependencies
+ */
+import { renderHook } from '@testing-library/react';
+
+/**
+ * Internal dependencies
+ */
+import {
+	SITE_EDITOR_NAVIGATE_EVENT,
+	ensureSiteEditorHistoryPatch,
+	useSiteEditorNavigate,
+} from '../history-patch';
+
+describe('ensureSiteEditorHistoryPatch', () => {
+	test('emits the navigate event on pushState and replaceState', () => {
+		window.history.replaceState({}, '', '/wp-admin/site-editor.php?p=/');
+		const listener = jest.fn();
+		window.addEventListener(SITE_EDITOR_NAVIGATE_EVENT, listener);
+
+		const unpatch = ensureSiteEditorHistoryPatch();
+		listener.mockClear();
+
+		window.history.pushState({}, '', '/wp-admin/site-editor.php?p=/styles');
+		expect(listener).toHaveBeenCalled();
+
+		listener.mockClear();
+		window.history.replaceState(
+			{},
+			'',
+			'/wp-admin/site-editor.php?p=/homepage'
+		);
+		expect(listener).toHaveBeenCalled();
+
+		unpatch();
+		window.removeEventListener(SITE_EDITOR_NAVIGATE_EVENT, listener);
+	});
+
+	test('unsubscribe removes the popstate emit added by this call', () => {
+		const listener = jest.fn();
+		window.addEventListener(SITE_EDITOR_NAVIGATE_EVENT, listener);
+
+		const unpatch = ensureSiteEditorHistoryPatch();
+		listener.mockClear();
+		unpatch();
+
+		window.dispatchEvent(new Event('popstate'));
+		expect(listener).not.toHaveBeenCalled();
+
+		window.removeEventListener(SITE_EDITOR_NAVIGATE_EVENT, listener);
+	});
+});
+
+describe('useSiteEditorNavigate', () => {
+	test('calls listener on subscribe and after patched pushState', () => {
+		window.history.replaceState({}, '', '/wp-admin/site-editor.php?p=/');
+		const listener = jest.fn();
+
+		const { unmount } = renderHook(() => useSiteEditorNavigate(listener));
+
+		expect(listener).toHaveBeenCalledTimes(1);
+
+		window.history.pushState({}, '', '/wp-admin/site-editor.php?p=/styles');
+		expect(listener).toHaveBeenCalledTimes(2);
+
+		unmount();
+	});
+
+	test('does not subscribe outside the Site Editor', () => {
+		window.history.replaceState({}, '', '/wp-admin/post.php?post=1');
+		const listener = jest.fn();
+
+		const { unmount } = renderHook(() => useSiteEditorNavigate(listener));
+
+		expect(listener).not.toHaveBeenCalled();
+		unmount();
+	});
+});
