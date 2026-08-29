5bb0cb96ac012fe9fb6b2754fcbfd7465a68900a
betteraliaghdam <betteraliaghdam@gmail.com>
2026-08-29 18:05:36 +0330

    fix(test): for latest data cleanup changes

--- packages/editor/js/extensions/libs/background/test/global-styles/background-position.global-styles-compatibility.e2e.cy.js
+++ packages/editor/js/extensions/libs/background/test/global-styles/background-position.global-styles-compatibility.e2e.cy.js
@@ -121,7 +121,7 @@ describe('Background Position → WP Compatibility (Global Styles)', () => {
 					cy.getByAriaLabel('Delete image 0').click({ force: true });
 				});
 
-				cy.wait(1000);
+				cy.wait(3000);
 
 				assertBlockData((data) => {
 					const root = getGroupGlobalStyles(data);
