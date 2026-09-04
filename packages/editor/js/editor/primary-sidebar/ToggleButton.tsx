d8750e2ae21d4256849a5791f1a4afe687819ef0
betteraliaghdam <betteraliaghdam@gmail.com>
2026-09-04 17:13:51 +0330

    feat(primary sidebar): update the icon

--- packages/editor/js/editor/primary-sidebar/ToggleButton.tsx
+++ packages/editor/js/editor/primary-sidebar/ToggleButton.tsx
@@ -6,22 +6,9 @@ import { __ } from '@wordpress/i18n';
 import { displayShortcut } from '@wordpress/keycodes';
 
 /**
- * SVG icon for the primary (right) sidebar toggle button.
+ * Blockera dependencies
  */
-const PrimarySidebarIcon = () => (
-	<svg
-		width="24"
-		height="24"
-		viewBox="0 0 24 24"
-		xmlns="http://www.w3.org/2000/svg"
-	>
-		<path
-			fillRule="evenodd"
-			clipRule="evenodd"
-			d="M18.5 4H6.5C5.4 4 4.5 4.9 4.5 6V18C4.5 19.1 5.4 20 6.5 20H18.5C19.6 20 20.5 19.1 20.5 18V6C20.5 4.9 19.6 4 18.5 4ZM14.5 5.5H6.5C6.2 5.5 6 5.7 6 6V18C6 18.3 6.2 18.5 6.5 18.5H14.5V5.5ZM18 18C18 18.3 17.8 18.5 17.5 18.5H15V5.5H17.5C17.8 5.5 18 5.7 18 6V18Z"
-		/>
-	</svg>
-);
+import { Icon } from '@blockera/icons';
 
 interface ToggleButtonProps {
 	isVisible: boolean;
@@ -43,7 +30,7 @@ export default function PrimaryToggleButton({
 	return (
 		<Button
 			data-test="blockera-primary-sidebar-toggle"
-			icon={<PrimarySidebarIcon />}
+			icon={<Icon icon="drawer-right" library="wp" />}
 			onClick={onToggle}
 			isPressed={isVisible}
 			label={label}
